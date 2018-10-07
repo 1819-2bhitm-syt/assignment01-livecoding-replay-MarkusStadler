96a07b764d4c1894308bd23f23bbe8dd0a8ebcd8
Markus Stadler <markus.stadler4092@gmail.com>
2018-10-07 21:14:15 +0200

    1.10.18 02SYT2web_asyncFunc_modules

---
+++ 1.10.18_Hausuebungen/02SYT2web_asyncFunc_modules/bsp007/app/app.js
@@ -0,0 +1,5 @@
+const data = require("./hallo.js");
+
+console.log(data.name("Hallo "));
+
+require("./unterordner/app.js");
---
+++ 1.10.18_Hausuebungen/02SYT2web_asyncFunc_modules/bsp008/app/app.js
@@ -0,0 +1,7 @@
+const data = require("./hallo.js");
+
+console.log(data.name("Hallo "));
+
+//require("./unterordner/app.js");
+
+require("./unterordner/index");
