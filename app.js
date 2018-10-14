6b14f510d461b470c290fbb90f13774be7a237ca
Markus Stadler <markus.stadler4092@gmail.com>
2018-10-14 14:23:21 +0200

    11.10.18 Zusammenfassung 1

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-let obj = { value: 10 };
+// The simplest way to create an object is using an object literal
+const circle = {
+    radius: 1,
+    draw: function() {}
+};
 
-function increase(obj) {
-    obj.value++;
+// To create multiple objects with the same structure and behaviour (methods),
+// use a factory or a constructor.
+
+// Factory function
+function createCircle(radius) {
+    return {
+        radius,
+        draw: function() {}
+    }
 }
 
-increase(obj);
-console.log(obj);
-
+// Constructor function
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function() {}
+}
