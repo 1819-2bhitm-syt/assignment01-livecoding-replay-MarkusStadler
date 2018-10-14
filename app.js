e768c544e29100de2be22d80b573e00e6bd579a6
Markus Stadler <markus.stadler4092@gmail.com>
2018-10-14 13:31:48 +0200

    11.10.18 Konstruktor 2

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Constructor Function
 function Circle(radius) {
+    console.log('this', this);
     this.radius = radius;
     this.draw = function() {
         console.log('draw');
@@ -7,3 +8,14 @@ function Circle(radius) {
 }
 
 const another = new Circle(1);
+
+// Constructor Function
+function Circle(radius) {
+    console.log('this', this);
+    this.radius = radius;
+    this.draw = function() {
+        console.log('draw');
+    };
+}
+
+const another = Circle(1);
