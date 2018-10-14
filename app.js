39fc4bafbd841f887dbf7ceb6c3506b772af4fae
Markus Stadler <markus.stadler4092@gmail.com>
2018-10-14 13:54:25 +0200

    11.10.18 Anzeige der erstellenden Funktion

--- app.js
+++ app.js
@@ -1,22 +1,9 @@
-let x = {};
-
-let name = '';  // single quotes
-let name = "";  // double quotes
-let name = ``;  // Backticks
+let name = 'Susi';
+console.log('name = ', name, name.constructor);
 
 let ok = true;
-let ok = false;
+console.log('ok = ', ok, ok.constructor);
 
-
-let total = 1;
-let total = 2;
-let total = 3;
-let total = 4;
-
-
-
-let x = new Object();       // Object Literal
-let name = new String();    // String Literals
-let ok = new Boolean();     // Boolean Literals
-let total = new Number();   // Number Literals
+let total = 100;
+console.log('total = ', total, total.constructor);
 
