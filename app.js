babbb8dd5dee4fb0f5c29561e2fe13bf0620f39f
Markus Stadler <markus.stadler4092@gmail.com>
2018-10-14 13:37:34 +0200

    11.10.18 Constructor Property

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+// Factory Function
+function createCircle(radius) {
+    return {
+        draw: function() {
+            console.log('draw');
+        }
+    };
+}
+
+const circle = createCircle(1);
+circle.draw();
+
 // Constructor Function
 function Circle(radius) {
     console.log('this', this);
@@ -8,14 +20,3 @@ function Circle(radius) {
 }
 
 const another = new Circle(1);
-
-// Constructor Function
-function Circle(radius) {
-    console.log('this', this);
-    this.radius = radius;
-    this.draw = function() {
-        console.log('draw');
-    };
-}
-
-const another = Circle(1);
