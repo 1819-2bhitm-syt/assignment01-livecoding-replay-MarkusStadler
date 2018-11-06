e61c1f5fc9ab376a020f6b1b6e8d981bb65e2ff0
Markus Stadler <markus.stadler4092@gmail.com>
2018-11-06 21:31:40 +0100

    5.11.18 Verwenden eines nodejs-Moduls

--- app.js
+++ app.js
@@ -1,25 +1,3 @@
-// to enumerate the members in an object:
-for (let key in circle) console.log(key, circle[key]);
+const os = require("os");
 
-Object.keys(circle);
-
-// To see if an object has a given property
-if ('location' in circle);
-
-// Abstraction means hiding the complexity/details and showing only the essentials.
-// We can hide the details by using private members. Replace "this" with "let".
-
-function Circle(radius) {
-    // Public member
-    this.radius = radius;
-
-    // Private member
-    let defaultLocation = {};
-}
-
-// To define a getter/setter, use Object.defineProperty():
-
-Object.defineProperty(this, 'defaultLocation', {
-    get: function() { return defaultLocation; },
-    set: function(value) { defaultLocation = value; }
-});
+console.log(os.cpus());
