090d5d128188583b331e696b86529e7f813d2c60
Markus Stadler <markus.stadler4092@gmail.com>
2018-11-11 21:52:32 +0100

    8.11.18 apply()

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ let logMessage = function(a, b) {
     console.log(this, a, b);
 };
 
+logMessage.apply({
+    vorname: "Max"
+}, [3, 4]);
+
+/*
 logMessage.call({
     vorname: "Max"
 }, 3, 4);
+ */
 
 /*
 let logThisBound = logThis.bind({
